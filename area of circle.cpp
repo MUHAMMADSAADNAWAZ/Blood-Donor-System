822fc96e2ccb5cef89bbe81a33874d9a5f3d1ab3
MUHAMMADSAADNAWAZ <105714937+MUHAMMADSAADNAWAZ@users.noreply.github.com>
2022-05-18 09:20:32 +0500

    Add files via upload

---
+++ area of circle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	double r;
+cout<<"enter first number:";
+cin>>r;
+// pie ka sign nhi mil rha isliye me pie ke sign ki jagaa g use kr rha hu saim value ke saath;
+double g=3.14;
+double a= g*r*r;
+cout<<"area of circle is =:"<<a;
+return 0;
+}
